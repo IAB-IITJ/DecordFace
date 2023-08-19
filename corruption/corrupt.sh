e8da7cbbcb31da03175baa898a8d3dea7018708b
RishiDarkDevil <rishi8001100192@gmail.com>
2023-08-19 13:20:23 +0530

    corrupt.sh explain

--- corruption/corrupt.sh
+++ corruption/corrupt.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # data path
+# Replace `INPUT_DATA_PATH` with the path to the directory which contains all the cropped and aligned images.
+# Replace `OUTPUT_DATA_PATH` with the path to the directory which corrupted images will be saved.
 input_data_path=INPUT_DATA_PATH
 output_data_path=OUTPUT_DATA_PATH
 
-# run detection and alignment
+# run corruption
+# for additional command line arguments check the argument parser of `corrupt-image-v3.py`
 python corrupt-image-v3.py \
     --indir_path $input_data_path \
     --outdir_path $output_data_path \
