02bcdeeec7fe446f13394e77caa63de44b497f85
RishiDarkDevil <rishi8001100192@gmail.com>
2023-08-19 14:20:40 +0530

    remove unecessary functions

--- corruption/imagenet_c/corruptions.py
+++ corruption/imagenet_c/corruptions.py
@@ -1,3 +1,4 @@
+# adopted and modified from: https://github.com/hendrycks/robustness/blob/master/ImageNet-C/imagenet_c/imagenet_c/corruptions.py
 # -*- coding: utf-8 -*-
 
 import numpy as np
@@ -230,65 +231,6 @@ def zoom_blur(x, severity=1):
 
     x = (x + out) / (len(c) + 1)
     return np.clip(x, 0, 1) * 255
-
-
-def fog(x, severity=1):
-    c = [(1.5, 2), (2., 2), (2.5, 1.7), (2.5, 1.5), (3., 1.4)][severity - 1]
-
-    x = np.array(x) / 255.
-    max_val = x.max()
-    x += c[0] * plasma_fractal(wibbledecay=c[1])[:224, :224][..., np.newaxis]
-    return np.clip(x * max_val / (max_val + c[0]), 0, 1) * 255
-
-
-def frost(x, severity=1):
-    c = [(1, 0.4),
-         (0.8, 0.6),
-         (0.7, 0.7),
-         (0.65, 0.7),
-         (0.6, 0.75)][severity - 1]
-    idx = np.random.randint(5)
-    filename = [resource_filename(__name__, 'frost/frost1.png'),
-                resource_filename(__name__, 'frost/frost2.png'),
-                resource_filename(__name__, 'frost/frost3.png'),
-                resource_filename(__name__, 'frost/frost4.jpg'),
-                resource_filename(__name__, 'frost/frost5.jpg'),
-                resource_filename(__name__, 'frost/frost6.jpg')][idx]
-    frost = cv2.imread(filename)
-    # randomly crop and convert to rgb
-    x_start, y_start = np.random.randint(0, frost.shape[0] - 224), np.random.randint(0, frost.shape[1] - 224)
-    frost = frost[x_start:x_start + 224, y_start:y_start + 224][..., [2, 1, 0]]
-
-    return np.clip(c[0] * np.array(x) + c[1] * frost, 0, 255)
-
-
-def snow(x, severity=1):
-    c = [(0.1, 0.3, 3, 0.5, 10, 4, 0.8),
-         (0.2, 0.3, 2, 0.5, 12, 4, 0.7),
-         (0.55, 0.3, 4, 0.9, 12, 8, 0.7),
-         (0.55, 0.3, 4.5, 0.85, 12, 8, 0.65),
-         (0.55, 0.3, 2.5, 0.85, 12, 12, 0.55)][severity - 1]
-
-    x = np.array(x, dtype=np.float32) / 255.
-    snow_layer = np.random.normal(size=x.shape[:2], loc=c[0], scale=c[1])  # [:2] for monochrome
-
-    snow_layer = clipped_zoom(snow_layer[..., np.newaxis], c[2])
-    snow_layer[snow_layer < c[3]] = 0
-
-    snow_layer = PILImage.fromarray((np.clip(snow_layer.squeeze(), 0, 1) * 255).astype(np.uint8), mode='L')
-    output = BytesIO()
-    snow_layer.save(output, format='PNG')
-    snow_layer = MotionImage(blob=output.getvalue())
-
-    snow_layer.motion_blur(radius=c[4], sigma=c[5], angle=np.random.uniform(-135, -45))
-
-    snow_layer = cv2.imdecode(np.fromstring(snow_layer.make_blob(), np.uint8),
-                              cv2.IMREAD_UNCHANGED) / 255.
-    snow_layer = snow_layer[..., np.newaxis]
-
-    x = c[6] * x + (1 - c[6]) * np.maximum(x, cv2.cvtColor(x, cv2.COLOR_RGB2GRAY).reshape(224, 224, 1) * 1.5 + 0.5)
-    return np.clip(x + snow_layer + np.rot90(snow_layer, k=2), 0, 1) * 255
-
 
 def spatter(x, severity=1):
     c = [(0.65, 0.3, 4, 0.69, 0.6, 0),
